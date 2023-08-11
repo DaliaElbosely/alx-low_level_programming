3bacaa7689d3f5572a45b74a58ad222d9abf1441
DaliaElbosely <da832230@gmail.com>
2023-08-10 18:02:47 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main
  *
- *Return - always 0
+ *Return: always 0
  */
 
 int main (void)
 {
-	puts("\|"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
